f1363b84156a0c317005425029fc39a3eacf718a
gino <lh1989216@gmail.com>
2021-10-05 15:14:25 +0800

    add classtest_0820.py

---
+++ classtest_0820.py
@@ -0,0 +1,51 @@
+print(100)
+print(1234)
+print("\tC:\"opp\tHELLO")
+print('GO')
+print('WRJGDF\'HNFDLGHNFDLVK\\\\SNKLJGNFJKGBNHSHT')
+print(1000==999)
+print('''100-99''')
+int=1
+v=123
+k=1.234
+l=10>0
+o="apple"
+print(o)
+print(99/10)
+print(99//10)
+print(2**10)
+print(2*10)
+print(99%10)
+print(2<1|1<0)
+print(99|10)
+print(99+True)
+print(99<10)
+print(100==10)#dds
+#mon
+x=10;
+y=1;
+z=1.2
+print(z)
+print(y)
+#eval函數將input()函數取得數值自行轉換數值型態
+po=eval(input("numeber:"))
+print(po*z)
+#sep' '和end' '輸出格式
+print(12,10.1,"yallow",sep="!!",end="\n")
+print("test",end="\t")
+print("test1",end="??")
+#格式化輸出
+"hello!%s" % "哈嘍"
+"%d除%d是%f"% (20,7,20/7)
+"%-5d除%5d是%.2f"% (20,7,20/7)
+"%5d除%-5d是%.3f"% (20,7,20/7)
+print("{x:10d}除以{y:<3d}等於{z:.5f}".format(x=10, y=3, z=10/3))
+print("{x:b}除以{y:b}等於{z:.5f}".format(x=10, y=3, z=10/3))
+print(78+12)
+
+#字串運算子
+print("卍"+"chicken"+"卍")
+#只能是正整數
+print("chicken "*3)
+pop="chicken"
+print(pop[2:-1])
